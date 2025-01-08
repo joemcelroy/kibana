c8d5603ba73bf7e6f1f3e60e4d85232b0f3ea1bb
Joseph McElroy <joseph.mcelroy@elastic.co>
2025-01-08 12:30:36 +0000

    support for inference connector in playground

--- x-pack/platform/packages/shared/kbn-inference-endpoint-ui-common/index.ts
+++ x-pack/platform/packages/shared/kbn-inference-endpoint-ui-common/index.ts
@@ -6,5 +6,7 @@
  */
 
 export { InferenceServiceFormFields } from './src/components/inference_service_form_fields';
+export { SERVICE_PROVIDERS } from './src/components/providers/render_service_provider/service_provider';
 
 export * from './src/types/types';
+export * from './src/constants';
